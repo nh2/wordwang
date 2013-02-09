ca2905e1d5b1c425589a4234daa59c4ed5357d72
Francesco Mazzoli <f@mazzo.li>
2013-02-09 16:01:20 +0000

    fix echo

--- server/Server.hs
+++ server/Server.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module Server where
 
-import           Control.Monad.Trans (liftIO)
+import           Control.Monad (forever)
 
+import           Control.Monad.Trans (liftIO)
 import           Data.Text (Text)
 import qualified Data.Text.IO as Text
 
 import           Network.WebSockets
 
 echo :: TextProtocol p => WebSockets p ()
-echo =
-    do dat :: Text <- receiveData
-       liftIO (Text.putStrLn dat)
-       sendTextData dat
+echo = forever $
+    do msg <- receiveData
+       liftIO (Text.putStrLn msg)
+       sendTextData (msg :: Text)
