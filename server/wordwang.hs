0f44d63056593bee7b0c2b03da22693439992c3b
Francesco Mazzoli <f@mazzo.li>
2013-02-09 15:48:17 +0000

    echo server

--- server/wordwang.hs
+++ server/wordwang.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import           Network.WebSockets (runServer, WebSockets, Hybi00)
+
+import           Server
+
 main :: IO ()
-main = do
-    putStrLn "Wanging the words"
+main = runServer "0.0.0.0" 8888 (const (echo :: WebSockets Hybi00 ()))
+
