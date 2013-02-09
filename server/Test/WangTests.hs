a5a4ebef7e76bdba340bddb856a39500d7ae488c
Alexandru Scvortov <scvalex@gmail.com>
2013-02-09 20:19:36 +0000

    flesh out test

--- server/Test/WangTests.hs
+++ server/Test/WangTests.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.Monoid ( mempty )
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
+import qualified Network.WebSockets as WS
+import Network.WebSockets ( WebSockets, Hybi00 )
+import Data.ByteString ( ByteString )
+import Server ( serve )
 
 main :: IO ()
 main = defaultMainWithOpts [testCase "expectedBehaviour" testExpectedBehaviour] options
@@ -12,4 +18,10 @@ main = defaultMainWithOpts [testCase "expectedBehaviour" testExpectedBehaviour] 
 
 testExpectedBehaviour :: Assertion
 testExpectedBehaviour = do
-    return ()
+    serve "localhost" 8765
+    WS.connect "ws://localhost" 8765 "" testWS
+  where
+    testWS :: WebSockets Hybi00 ()
+    testWS = do
+        WS.sendTextData ("Hello" :: ByteString)
+        return ()
