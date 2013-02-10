cdff30a1d4f06ab0cbaa12ba09c62f69da0b13a0
Alexandru Scvortov <scvalex@gmail.com>
2013-02-10 22:08:53 +0000

    add logging to main program

--- server/wordwang.hs
+++ server/wordwang.hs
@@ -2,17 +2,50 @@
 module Main ( main ) where
 
 import Control.Concurrent.MVar ( newEmptyMVar, putMVar, takeMVar )
-import Server
+import Server ( serve )
+import System.FilePath ( (</>) )
+import System.IO ( stdout )
+import System.Log.Handler.Simple ( verboseStreamHandler, fileHandler )
+import System.Log.Logger ( infoM, Priority(..)
+                         , updateGlobalLogger, setHandlers, setLevel, rootLoggerName )
 import System.Posix.Signals ( Handler(..), installHandler, sigINT )
+import Text.Printf ( printf )
 import qualified Control.Exception as CE
+
+------------------------------------------
+-- Constants
+------------------------------------------
+
+tag :: String
+tag = "wordwang"
+
+host :: String
+host = "0.0.0.0"
+
+port :: Int
+port = 8888
+
+------------------------------------------
+-- Functions
+------------------------------------------
 
 main :: IO ()
 main = do
-    shutdown <- serve "0.0.0.0" 8888
+    setupLogging
+    infoM tag "starting wordwang"
+    shutdown <- serve host port
+    infoM tag (printf "serving on %s:%d" host port)
     done <- newEmptyMVar
     _ <- installHandler sigINT (Catch $ putMVar done ()) Nothing
     takeMVar done `CE.finally` do
-        _ <- putStrLn "Shutting down... "
+        infoM tag "shutting down"
         shutdown
-        _ <- putStrLn "done"
-        return ()
+        infoM tag "shutdown done"
+
+setupLogging :: IO ()
+setupLogging = do
+    consoleH <- verboseStreamHandler stdout DEBUG
+    fileH <- fileHandler ("log" </> "server.log") DEBUG
+    updateGlobalLogger rootLoggerName $ setLevel DEBUG
+    updateGlobalLogger rootLoggerName $ \logger ->
+        flip setHandlers logger [consoleH, fileH]
