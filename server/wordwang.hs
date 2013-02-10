79b6fa1d875da3894a9332d08ab4ab22b07648e4
Alexandru Scvortov <scvalex@gmail.com>
2013-02-10 21:38:38 +0000

    reformat wordwang

--- server/wordwang.hs
+++ server/wordwang.hs
@@ -1,17 +1,18 @@
-import Server
+-- | Command line tool to start the wordwang server.
+module Main ( main ) where
 
-import           Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent.MVar ( newEmptyMVar, putMVar, takeMVar )
+import Server
+import System.Posix.Signals ( Handler(..), installHandler, sigINT )
 import qualified Control.Exception as CE
 
-import           System.Posix.Signals (Handler(..), installHandler, sigINT)
-
 main :: IO ()
-main =
-    do shutdown <- serve "0.0.0.0" 8888
-       done <- newEmptyMVar
-       _ <- installHandler sigINT (Catch $ putMVar done ()) Nothing
-       takeMVar done `CE.finally` (
-           do _ <- putStrLn "Shutting down... "
-              shutdown
-              _ <- putStrLn "done"
-              return ())
+main = do
+    shutdown <- serve "0.0.0.0" 8888
+    done <- newEmptyMVar
+    _ <- installHandler sigINT (Catch $ putMVar done ()) Nothing
+    takeMVar done `CE.finally` do
+        _ <- putStrLn "Shutting down... "
+        shutdown
+        _ <- putStrLn "done"
+        return ()
